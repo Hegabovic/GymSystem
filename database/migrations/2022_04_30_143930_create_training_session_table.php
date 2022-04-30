4c77efce513923781fefba3ea0a27b0acb334a09
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-04-30 17:01:39 +0200

    create structure of training session table

--- database/migrations/2022_04_30_143930_create_training_session_table.php
+++ database/migrations/2022_04_30_143930_create_training_session_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('training_session', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->date('start_at');
+            $table->date('finish_at');
             $table->timestamps();
         });
     }
