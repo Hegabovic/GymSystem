14409dd56dfc187cd08fc7135448788d7a5d50c1
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-04-30 16:24:11 +0200

    create structure of gym table consist of id,name,coverimage,userid

--- database/migrations/2022_04_30_131323_create_gyms_table.php
+++ database/migrations/2022_04_30_131323_create_gyms_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
         Schema::create('gyms', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->date('created_at');
+            $table->string("cover_image");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
