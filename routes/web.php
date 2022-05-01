fdec6291e89c7204ae6605456d99712f721baf01
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-01 02:53:41 +0200

    Fix Routes

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Auth::routes();
