b2b0b1b8a566763efe5a3918adea50d2c19016ae
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-01 16:52:48 +0200

    Create Couches table and fix migration order of couches table

--- database/migrations/2022_04_28_144240_create_couches_table.php
+++ database/migrations/2022_04_28_144240_create_couches_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,11 +12,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('training_session', function (Blueprint $table) {
+        Schema::create('couches', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->date('start_at');
-            $table->date('finish_at');
+            $table->string('name')->nullable(false);
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('training_session');
+        Schema::dropIfExists('couches');
     }
 };
