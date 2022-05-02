d254f41dddf346b8c24251a890b42c10d902389d
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-05-02 15:00:27 +0200

    make model Gym

--- app/Http/Controllers/GymController.php
+++ app/Http/Controllers/GymController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Gym;
 
 use Illuminate\Http\Request;
 
 class GymController extends Controller
 {
-    //
+    public function AvailbleGymes()
+    {
+        $gymes= Gym::All();
+        return view('layout.AvailableGymes', [
+                'gymes'=>$gymes,
+            ]);
+
+            // if($input['role']==='city-manager') $user->assignRole('CityManager');
+            // elseif ($input['role']==='gym-manager') $user->assignRole('GymManager');
+    }
 }
