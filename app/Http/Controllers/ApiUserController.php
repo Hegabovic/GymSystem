c9fd143fc08e730bbb8653479ceaa3323d972c70
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-05-04 19:37:13 +0200

    before pull

---
+++ app/Http/Controllers/ApiUserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CustomerRegisterRequest;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+
+class ApiUserController extends Controller
+{
+    public function register(CustomerRegisterRequest $request)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CityController;
+use App\Http\Controllers\ApiUserController;
 use App\Http\Controllers\CoachController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/customer/register',[ApiUserController::class,'register'])->name('customer-register');
+
+Route::get('coaches-table', [CoachController::class, 'showCoachesTable'])->name('show_coaches_table');
 Route::delete('/attendance-delete', 'App\Http\Controllers\attendanceController@delete')->name('delete.attendances');
+
+
+
+
+
 Route::delete('/city-delete', [CityController::class, 'delete'])->name('delete.city');
 Route::put('/city-restore',[CityController::class,'restore'])->name('restore.city');
 Route::delete('/coach-delete', [CoachController::class, 'delete'])->name('coach.delete');
