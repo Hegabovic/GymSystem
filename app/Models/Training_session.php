73ba49f31fcd45326330c75bb5e7db91e3ffe614
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-05 03:34:01 +0200

    Fix Coach model relation with sessionCoaches and create trainingSessions relations

--- app/Models/Training_session.php
+++ app/Models/Training_session.php
@@ -4,10 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Training_session extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function sessionsCoaches(): HasMany
+    {
+        return $this->hasMany(SessionsCoaches::class);
+    }
+
+    public function attendaces(): HasMany
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    protected $fillable = [
+        'name',
+        'gym_id',
+        'start_at',
+        'finish_at'
+    ];
+
+
 }
