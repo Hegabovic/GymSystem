0f265a9fd9c94c5afd8e7056d38b2b1a95905653
hegabovic <abdallahhegab192@gmail.com>
2022-05-05 22:42:17 +0200

    added routes for palns

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Spatie\Permission\Contracts\Role;
 Route::group(['middleware'=>'auth'],function (){
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-  
+
     Route::get('/create-city-manager', [UserController::class, 'createCityManager'])->name('create_city_manager');
     Route::get('/create-gym-manager', [UserController::class, 'createGymManager'])->name('create_gym_manager');
     Route::get('/show-users', [UserController::class, 'showUsers'])->name('show_users');
@@ -47,19 +47,19 @@ Route::group(['middleware'=>'auth'],function (){
     Route::post('/coach-store', [CoachController::class, 'store'])->name('store_coach');
     Route::get('/coach-edit/{id}', [CoachController::class, 'edit'])->name('update_coach');
     Route::put('/coach-update/{id}', [CoachController::class, 'storeEdit'])->name('store_updated_coach');
-  
+
     Route::get('/trainingSession', [TrainingSessionController::class, 'index'])->name('show_trainingSessions');
     Route::get('/trainingSession-create', [TrainingSessionController::class, 'create'])->name('create_trainingSession');
     Route::post('/trainingSession-store', [TrainingSessionController::class, 'store'])->name('store_trainingSession');
     Route::get('/trainingSession-edit/{id}', [TrainingSessionController::class, 'edit'])->name('update_trainingSession');
     Route::put('/trainingSession-update/{id}', [TrainingSessionController::class, 'storeEdit'])->name('store_updated_trainingSession');
-  
+
     Route::get('/city', [CityController::class, 'index'])->name('show_cities');
     Route::get('/city-create', [CityController::class, 'create'])->name('create');
     Route::post('/city-store', [CityController::class, 'store'])->name('store_city');
     Route::get('/edit/{id}', [CityController::class, 'edit'])->name('city.edit');
     Route::put('/update/{id}', [CityController::class, 'update'])->name('city.update');
-  
+
     Route::get('/attendance', [attendanceController::class, 'show'])->name('show.attendances');
     Route::get('/attendance-create', [attendanceController::class, 'create'])->name('create.attendances');
     Route::post('/attendance-store', [attendanceController::class, 'store'])->name('store.attendances');
@@ -74,7 +74,12 @@ Route::group(['middleware'=>'auth'],function (){
     Route::post ('/packages',[PackageController::class,'store'])->name('packages.store');
     Route::get('/packages-edit/{id}',[PackageController::class, 'edit'])->name('packages.edit');
     Route::put('/packages-update/{id}',[PackageController::class, 'update'])->name('packages.update');
-   
+
+    Route::get('/plans' , [PlanController::class, 'index'])->name('plans.show');
+    Route::get('/plans/{plan}', [PlanController::class, 'show'])->name('show.plan');
+    Route::post('/subscription', [SubscriptionController::class, 'create'])->name('subscription.create');
+
+
 });
 
 Auth::routes();
