ed6f5d9afaa1ce1bfecfccb51a63d13b9cfc52d1
Adham <adhamahmed1234321@gmail.com>
2022-05-05 17:14:18 +0200

    attendance (merging problem) edit button problem solved

--- resources/views/attendance/show_attendance.blade.php
+++ resources/views/attendance/show_attendance.blade.php
@@ -66,6 +66,8 @@
                                 </thead>
                                 <tbody>
                                 @foreach ($items as $table)
+                                     @if ( ! $table->trashed())
+
                                     <tr align="center">
                                         <td>{{$table->id}}</td>
                                         <td>{{$table->customer->user->name}}</td>
@@ -83,8 +85,13 @@
                                         <td>{{$table->gym->city->name}}</td>
                                         <td>
                                             <button class="btn btn-danger delete" id="{{$table->id}}"><i class="fas fa-trash-alt"></i></button>
+                                            {{-- <button class="btn btn-primary edit" edit-id="{{$table->id}}"><i class="fas fa-edit"></i></button> --}}
+                                            <a href="{{ route('edit.attendances',  $table->id) }}" class="btn btn-primary edit"><i class="fas fa-edit"></i>
+                                            </a>
+
                                         </td>
                                     </tr>
+                                    @endif
                                 @endforeach
 
                                 <script>
@@ -121,3 +128,169 @@
         </section>
     </div>
 @endsection
+
+
+{{-- ######################### --}}
+
+{{-- 
+@extends('layouts.app')
+@section('content')
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        </div>
+
+
+        <!-- Content Wrapper. Contains page content -->
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard v2</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v2</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Info boxes -->
+
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Attendances</h5>
+
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-tool dropdown-toggle"
+                                                data-toggle="dropdown">
+                                            <i class="fas fa-wrench"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-right" role="menu">
+                                            <a href="#" class="dropdown-item">Action</a>
+                                            <a href="#" class="dropdown-item">Another action</a>
+                                            <a href="#" class="dropdown-item">Something else here</a>
+                                            <a class="dropdown-divider"></a>
+                                            <a href="#" class="dropdown-item">Separated link</a>
+                                        </div>
+                                    </div>
+                                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+
+                            <!-- ./card-body -->
+
+            <table id="example" class="table table-bordered" style="color: black">
+                 <thead>
+                    <tr align="center">
+                        <th>Id</th>
+                        <th>user name</th>
+                        <th>email</th>
+                        <th>training session name</th>
+                        <th>attendance date</th>
+                        <th>attendance time</th>
+                        <th>Gym</th>
+                        <th>City</th>
+                        <th>options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($items as $table)
+                    @if ( ! $table->trashed())
+                    <tr align="center">
+                        <td>{{$table->id}}</td> 
+                        <td>{{$table->customer->user->name}}</td> 
+                        <td>{{$table->customer->user->email}}</td> 
+                        <td>{{$table->training_session->name}}</td> 
+                        
+                        @if($table->created_at)
+                        <td>{{$table->created_at->toDateString() }}</td> 
+                        @endif
+                        
+                        @if($table->created_at)
+                        <td>{{$table->created_at->format('H:i')}}</td> 
+                        @endif
+                        <td>{{$table->gym->name}}</td> 
+                        <td>{{$table->gym->city->name}}</td> 
+                      
+                        <td>
+                            <button class="btn btn-danger delete" id="{{$table->id}}"><i class="fas fa-trash-alt"></i></button>
+                            <button class="btn btn-primary edit" id="{{$table->id}}"><i class="fas fa-edit"></i></button>
+                        </td>
+                    </tr>
+                  
+                    @else
+            @endif
+                    @endforeach
+
+                    <script > 
+                     $(document).on('click', '.delete', function() {
+                        let confirm=confirm('Are you sure you want to delete ?');
+                        if(confirm)
+                        {
+                            let myThis=$(this).parent().parent()
+                            $(this).parent().parent().css("background-color", "grey");
+
+                            let Attendance_id=this.getAttribute('data-id');
+                            $.ajax({
+                                url: "{{route('delete.attendances')}}"+`?id=${Attendance_id}`,
+                                type: 'DELETE',
+                                contentType: 'application/json',
+                                data: `{"id":"${Attendance_id}"}`,
+                                success: function(result) {
+                                    myThis.remove();
+                                }
+                            });
+                        }
+                    });
+                    </script>
+                    
+                <tbody> 
+               
+               
+             </table>
+
+             
+             <script>
+                $(document).ready(function() {
+                 $('#example').DataTable();
+             } );
+                </script>  
+                
+                            <!-- /.card-footer -->
+                        </div>
+                        <!-- /.card -->
+
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+                <!-- Main row -->
+
+                <!-- /.row -->
+            </div><!--/. container-fluid -->
+        </section>
+    </div>
+@endsection --}}
