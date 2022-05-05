69b7b15da48b1d7fa902ec493f954d0951909794
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-05 19:15:31 +0200

    change date time formate to d-m-Y h:iA using carbon model

--- resources/views/trainingSessions/edit.blade.php
+++ resources/views/trainingSessions/edit.blade.php
@@ -11,12 +11,12 @@
             <div class="form-group">
                 <label for="StartAt">Start at</label>
                 <input type="date" name="startAt" class="form-control" id="StartAt"
-                       value="{{$trainingSession->start_at}}">
+                       value="{{$trainingSession->start_at->format('d-m-Y h:iA')}}">
             </div>
             <div class="form-group">
                 <label for="FinishAt">Finish at</label>
                 <input type="date" name="finishAt" class="form-control" id="FinishAt"
-                       value="{{$trainingSession->finish_at}}">
+                       value="{{$trainingSession->finish_at->format('d-m-Y h:iA')}}">
             </div>
         </div>
 
