689f5736b7546e8e40beddd2b518fb97830c65f0
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-06 15:57:07 +0200

    use repository pattern in AttendanceController

--- app/Http/Controllers/attendanceController.php
+++ app/Http/Controllers/attendanceController.php
@@ -2,75 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\AttendanceRepositoryInterface;
+use App\Contracts\TrainingSessionsRepositoryInterface;
 use Illuminate\Http\Request;
-use App\Models\Attendance;
-use App\Models\Training_session;
-use Illuminate\Support\Facades\View;
 
 class attendanceController extends Controller
 {
-    public function show(){
-        $tableData=Attendance::withTrashed()->get();
-        return view('attendance/show_attendance',['items'=>$tableData,'userData'=>request()->user()]);
+
+    private AttendanceRepositoryInterface $attendanceRepository;
+    private TrainingSessionsRepositoryInterface $trainingSessionsRepository;
+
+    public function __construct(AttendanceRepositoryInterface $attendanceRepository, TrainingSessionsRepositoryInterface $trainingSessionsRepository)
+    {
+        $this->attendanceRepository = $attendanceRepository;
+        $this->trainingSessionsRepository = $trainingSessionsRepository;
     }
 
-    public function create(){
+    public function show()
+    {
+        $attendances = $this->attendanceRepository->all();
+        return view('attendance/show_attendance', ['items' => $attendances, 'userData' => request()->user()]);
+    }
+
+    public function create()
+    {
         return view('attendance/create_attendance');
     }
 
-    public function store(Request $request){
-        $data=request()->all(); 
-        $userData=$request->user();
-        $attendance=Attendance::create(
+    public function store(Request $request)
+    {
+        $data = request()->all();
+        $userData = $request->user();
+        $attendance = $this->attendanceRepository->create(
             [
-                'customer_id' =>$userData['id'],
-                'gym_id' =>$data['gym_id'],
-                'training_session_id' =>$data['training_session_name'],
+                'customer_id' => $userData['id'],
+                'gym_id' => $data['gym_id'],
+                'training_session_id' => $data['training_session_name'],
 
             ]);
         return to_route('show.attendances');
     }
+
     public function restore()
     {
-        $data=request()->input('id'); 
-        $post = Attendance::withTrashed()->where('id', $data)->first();
+        $data = request()->input('id');
+        $post = $this->attendanceRepository->allWithTrashed()->where('id', $data)->first();
         $post->restore();
         $post->save();
         return to_route('show.attendances');
     }
+
     public function delete()
     {
-        $isDeleted=false;
-        $data=request()->input('id'); 
-        $record=Attendance::find($data);
-        if($record)
-        {
-            $isDeleted=$record->delete();
+        $attendanceId = request()->input('id');
+
+        $result = $this->attendanceRepository->delete($attendanceId);
+
+        if ($result) {
+            return ['success' => 'true'];
         }
-        if($isDeleted)
-        {
-            return ['success' =>'true'];
-        }else{
-            return ['success' =>'false'];
-        }
+
+        return ['success' => 'false', 'message' => "Couldn't delete selected attendance successfully"];
     }
+
     public function edit($id)
     {
-        $attendance=Attendance::All();
-        $thisAttendance=Attendance::find($id);
+        $attendances = $this->attendanceRepository->all();
+        $selectedAttendance = $this->attendanceRepository->findById($id);
 
-       return view('attendance/editAttendance',['attendances'=>$attendance,'thisAttendance'=>$thisAttendance]);
+        return view('attendance/editAttendance', ['attendances' => $attendances, 'thisAttendance' => $selectedAttendance]);
     }
 
     public function update(Request $request, $attendance_id)
     {
-        $attendance = Attendance::find($attendance_id);
-        $training_session = Training_session::find($attendance_id);
-        $attendance->update([
+        $training_session = $this->trainingSessionsRepository->findById($attendance_id);
+        $this->attendanceRepository->update($attendance_id, [
             "customer_id" => $request->user,
             "gym_id" => $request->gym,
             "training_session_id" => $request->training_session,
         ]);
         return to_route('show.attendances');
     }
-}   
+}
