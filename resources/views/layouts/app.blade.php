9478989b93e7f2f88f08f966592cfeeed53be757
hegabovic <abdallahhegab192@gmail.com>
2022-05-07 05:26:51 +0200

    fixed ui <show create>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -349,18 +349,22 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="{{route('create_trainingSession')}}" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Create</p>
-                                </a>
-                            </li>
+
                             <li class="nav-item">
                                 <a href="{{route('show_trainingSessions')}}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Show</p>
                                 </a>
                             </li>
+
+                            <li class="nav-item">
+                                <a href="{{route('create_trainingSession')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Create</p>
+                                </a>
+                            </li>
+
+
                         </ul>
                     </li>
                     </li>
@@ -374,17 +378,19 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="{{route('create_coach')}}" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Create</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
                                 <a href="{{route('show_coaches')}}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Show</p>
                                 </a>
                             </li>
+
+                            <li class="nav-item">
+                                <a href="{{route('create_coach')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Create</p>
+                                </a>
+                            </li>
+
                         </ul>
                     </li>
                     <li class="nav-header">SETTINGS</li>
