66e63495a1b0e80a74956aed72ebf8b99674aacf
hegabovic <abdallahhegab192@gmail.com>
2022-05-07 05:56:39 +0200

    fixed ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,19 +55,20 @@
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="{{ route('home') }}" class="nav-link">Home</a>
             </li>
+
+            <li class="nav-item">
+                <a href="{{route('edit_profile')}}" class="nav-link">
+                    <p>
+                        Profile
+                    </p>
+                </a>
+            </li>
+
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="{{ route('contact') }}" class="nav-link">Contact</a>
             </li>
 
-            <li class="nav-item">
-                <a href="{{route('edit_profile')}}" class="nav-link">
-                    <i class="nav-icon fas fa-calendar-alt"></i>
-                    <p>
-                        Profile
-                        <span class="badge badge-info right">2</span>
-                    </p>
-                </a>
-            </li>
+
         </ul>
 
         <!-- Right navbar links -->
@@ -122,7 +123,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ route('home') }}" class="brand-link">
-            <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            <img src="{{Storage::url(Auth::user()->avatar_path)}}" alt="admin image" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">Gym Jutsu</span>
         </a>
