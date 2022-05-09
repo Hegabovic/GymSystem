addb4fd7cd4ee73fafb655db3a46f1cafe8f4e09
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-05-09 23:50:51 +0200

    adding with trashed

--- app/Models/GymManager.php
+++ app/Models/GymManager.php
@@ -24,6 +24,7 @@ class GymManager extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id')->withTrashed();
+
     }
 }
