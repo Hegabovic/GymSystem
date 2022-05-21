925ad9bdb34e4e94db57f45b62da86aa597acaee
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-05-21 20:58:49 +0200

    Fix Gym Managers table style

--- resources/views/coaches/show.blade.php
+++ resources/views/coaches/show.blade.php
@@ -6,90 +6,88 @@
                 <div class="row mb-2">
                     <div class="col-sm-6">
                         <h1 class="m-0">Show Coaches</h1>
-                    </div><!-- /.col -->
+                    </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                             <li class=" breadcrumb-item active">Show Coaches
                             </li>
                         </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div>
-            <div class="row">
-                <div class="col-md-12 ">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <table class="table table-bordered" style="color: black" id="datatable">
-                                <thead>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 ">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <table class="table table-bordered" style="color: black" id="datatable">
+                                    <thead>
 
-                                <tr>
-                                    <th style="text-align: center;">ID</th>
-                                    <th style="text-align: center;">Name</th>
-                                    <th style="text-align: center;">Phone</th>
-                                    <th style="text-align: center;">Address</th>
-                                    <th style="text-align: center;">Options</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($coaches as $coach)
                                     <tr>
-                                        <td style="text-align: center;">{{$coach->id}}</td>
-                                        <td style="text-align: center;">{{$coach->name}}</td>
-                                        <td style="text-align: center;">{{$coach->phone}}</td>
-                                        <td style="text-align: center;">{{$coach->address}}</td>
-                                        <td style="text-align: center;">
-                                            <a role="button" href="{{route('update_coach',[$coach->id])}}"
-                                               class="btn btn-primary m-1 d-inline-block"
-                                               data-id="{{$coach->id}}">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
-                                            @can('permission_delete_couch')
-                                                <a role="button" class="btn btn-danger m-1 d-inline-block delete"
-                                                   data-id="{{$coach->id}}">
-                                                    <i class="fas fa-trash-alt"></i>
-                                                </a>
-                                            @endcan
-                                        </td>
+                                        <th style="text-align: center;">ID</th>
+                                        <th style="text-align: center;">Name</th>
+                                        <th style="text-align: center;">Phone</th>
+                                        <th style="text-align: center;">Address</th>
+                                        <th style="text-align: center;">Options</th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($coaches as $coach)
+                                        <tr>
+                                            <td style="text-align: center;">{{$coach->id}}</td>
+                                            <td style="text-align: center;">{{$coach->name}}</td>
+                                            <td style="text-align: center;">{{$coach->phone}}</td>
+                                            <td style="text-align: center;">{{$coach->address}}</td>
+                                            <td style="text-align: center;">
+                                                <a role="button" href="{{route('update_coach',[$coach->id])}}"
+                                                   class="btn btn-primary m-1 d-inline-block"
+                                                   data-id="{{$coach->id}}">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                @can('permission_delete_couch')
+                                                    <a role="button" class="btn btn-danger m-1 d-inline-block delete"
+                                                       data-id="{{$coach->id}}">
+                                                        <i class="fas fa-trash-alt"></i>
+                                                    </a>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <script>
-            $(document).ready(function () {
-                $('#datatable').DataTable();
-            });
-
-            sendDeleteRequest();
-
-            function sendDeleteRequest() {
-                $(document).on('click', '.delete', function (event) {
-                    event.preventDefault();
-                    let coachId = this.getAttribute('data-id');
-                    let url = "{{route('coach.delete')}}" + `?id=${coachId}`;
-                    let result = confirm('Are you sure you want to delete ?');
-                    if (result) {
-                        let row = $(this).parent().parent();
-                        $.ajax({
-                            url: url,
-                            type: 'DELETE',
-                            success: function (result) {
-                                if (result.success)
-                                    row.remove();
-                                else
-                                    alert(result.message);
-                            }
-                        });
-                    }
+            <script>
+                $(document).ready(function () {
+                    $('#datatable').DataTable();
                 });
-            }
 
-        </script>
+                sendDeleteRequest();
+
+                function sendDeleteRequest() {
+                    $(document).on('click', '.delete', function (event) {
+                        event.preventDefault();
+                        let coachId = this.getAttribute('data-id');
+                        let url = "{{route('coach.delete')}}" + `?id=${coachId}`;
+                        let result = confirm('Are you sure you want to delete ?');
+                        if (result) {
+                            let row = $(this).parent().parent();
+                            $.ajax({
+                                url: url,
+                                type: 'DELETE',
+                                success: function (result) {
+                                    if (result.success)
+                                        row.remove();
+                                    else
+                                        alert(result.message);
+                                }
+                            });
+                        }
+                    });
+                }
+            </script>
 @endsection
 
 
--- resources/views/gymManagers/show.blade.php
+++ resources/views/gymManagers/show.blade.php
@@ -1,82 +1,94 @@
 @extends('layouts.app')
 @section('content')
     <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 ">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Gyms Mangers</div>
-                        <div class="panel-body">
-                            <table class="table table-bordered" style="color: black;" id="datatable">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>National ID</th>
-                                    <th>Options</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($managers as $manager)
-                                    @if(!$manager->trashed())
-                                        <tr>
-                                            <td>{{$manager->user_id}}</td>
-                                            <td>{{$manager->user->name}}</td>
-                                            <td>{{$manager->n_id}}</td>
-                                            <td>
-                                                <a role="button" href="{{route('edit_gymManger',[$manager->id])}}"
-                                                   class="btn btn-primary m-1 d-inline-block"
-                                                   data-id="{{$manager->id}}">
-                                                    <i class="fas fa-edit"></i>
-                                                </a>
-                                                <a role="button" class="btn btn-danger m-1 d-inline-block delete"
-                                                   data-id="{{$manager->id}}">
-                                                    <i class="fas fa-trash-alt"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    @else
-                                        <tr>
-                                            <td colspan="5" class="text-center">
-                                                <a role="button"
-                                                   class="btn btn-warning m-1 d-inline-block restore"
-                                                   data-id="{{$manager->id}}">
-                                                    <i class="fas fa-redo"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                                </tbody>
-                            </table>
+        <div class="container-fluid p-5">
+            <div class="content-header">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Show Gyms Managers</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class=" breadcrumb-item active">Show Gyms Managers
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 ">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <table class="table table-bordered" style="color: black;" id="datatable">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>National ID</th>
+                                        <th>Options</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($managers as $manager)
+                                        @if(!$manager->trashed())
+                                            <tr>
+                                                <td>{{$manager->user_id}}</td>
+                                                <td>{{$manager->user->name}}</td>
+                                                <td>{{$manager->n_id}}</td>
+                                                <td>
+                                                    <a role="button" href="{{route('edit_gymManger',[$manager->id])}}"
+                                                       class="btn btn-primary m-1 d-inline-block"
+                                                       data-id="{{$manager->id}}">
+                                                        <i class="fas fa-edit"></i>
+                                                    </a>
+                                                    <a role="button" class="btn btn-danger m-1 d-inline-block delete"
+                                                       data-id="{{$manager->id}}">
+                                                        <i class="fas fa-trash-alt"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @else
+                                            <tr>
+                                                <td colspan="5" class="text-center">
+                                                    <a role="button"
+                                                       class="btn btn-warning m-1 d-inline-block restore"
+                                                       data-id="{{$manager->id}}">
+                                                        <i class="fas fa-redo"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script>
-        $(document).ready(function () {
-            $('#datatable').DataTable();
-        });
+        <script>
+            $(document).ready(function () {
+                $('#datatable').DataTable();
+            });
 
-        sendDeleteRequest();
+            sendDeleteRequest();
 
-        function sendDeleteRequest() {
-            $(document).on('click', '.delete', function (event) {
-                event.preventDefault();
-                let gymMangerId = this.getAttribute('data-id');
-                let url = "{{route('gymManger.delete')}}" + `?id=${gymMangerId}`;
-                let result = confirm('Are you sure you want to ban ?');
-                if (result) {
-                    let row = $(this).parent().parent();
-                    $.ajax({
-                        url: url,
-                        type: 'DELETE',
-                        success: function (result) {
-                            if (result.success) {
-                                console.log(row);
-                                let newRow = `<tr>
+            function sendDeleteRequest() {
+                $(document).on('click', '.delete', function (event) {
+                    event.preventDefault();
+                    let gymMangerId = this.getAttribute('data-id');
+                    let url = "{{route('gymManger.delete')}}" + `?id=${gymMangerId}`;
+                    let result = confirm('Are you sure you want to ban ?');
+                    if (result) {
+                        let row = $(this).parent().parent();
+                        $.ajax({
+                            url: url,
+                            type: 'DELETE',
+                            success: function (result) {
+                                if (result.success) {
+                                    console.log(row);
+                                    let newRow = `<tr>
                                             <td colspan="5" class="text-center">
                                                 <a role="button"
                                                    class="btn btn-warning m-1 d-inline-block restore"
@@ -85,35 +97,35 @@
                                                 </a>
                                             </td>
                                         </tr>`
-                                row.remove();
-                                $("tbody").append(newRow);
-                            } else
-                                alert(result.message);
-                        }
-                    });
-                }
-            });
-        }
+                                    row.remove();
+                                    $("tbody").append(newRow);
+                                } else
+                                    alert(result.message);
+                            }
+                        });
+                    }
+                });
+            }
 
-        sendRestoreRequest();
+            sendRestoreRequest();
 
-        function sendRestoreRequest() {
-            $(document).on('click', '.restore', function (event) {
-                event.preventDefault();
-                let gymMangerId = this.getAttribute('data-id');
-                let url = "{{route('gymManger.restore')}}" + `?id=${gymMangerId}`;
-                let deleteUrl = "{{route('gymManger.delete')}}" + `?id=${gymMangerId}`;
-                let result = confirm('Are you sure you want to unban ?');
+            function sendRestoreRequest() {
+                $(document).on('click', '.restore', function (event) {
+                    event.preventDefault();
+                    let gymMangerId = this.getAttribute('data-id');
+                    let url = "{{route('gymManger.restore')}}" + `?id=${gymMangerId}`;
+                    let deleteUrl = "{{route('gymManger.delete')}}" + `?id=${gymMangerId}`;
+                    let result = confirm('Are you sure you want to unban ?');
 
-                if (result) {
-                    let row = $(this).parent().parent();
-                    $.ajax({
-                        url: url,
-                        type: 'DELETE',
-                        success: function (result) {
-                            if (result.success) {
-                                console.log(result);
-                                let newRow = `
+                    if (result) {
+                        let row = $(this).parent().parent();
+                        $.ajax({
+                            url: url,
+                            type: 'DELETE',
+                            success: function (result) {
+                                if (result.success) {
+                                    console.log(result);
+                                    let newRow = `
                                         <tr>
                                             <td>${gymMangerId}</td>
                                             <td>${result.manager.name}</td>
@@ -130,16 +142,16 @@
                                                 </a>
                                             </td>
                                         </tr>`
-                                row.remove();
-                                $("tbody").append(newRow);
-                            } else
-                                alert(result.message);
-                        }
-                    });
-                }
-            });
-        }
-    </script>
+                                    row.remove();
+                                    $("tbody").append(newRow);
+                                } else
+                                    alert(result.message);
+                            }
+                        });
+                    }
+                });
+            }
+        </script>
 @endsection
 
 
